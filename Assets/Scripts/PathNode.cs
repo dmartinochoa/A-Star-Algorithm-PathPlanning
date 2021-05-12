e87557e3d5cccb60ea32fe97c9fadf74d5693d9a
dmartinochoa <56157528+dmartinochoa@users.noreply.github.com>
2021-05-12 12:35:45 +0200

    algorithm working
    shows path with debug log

---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode
+{
+    private GridSystem<PathNode> grid;
+
+    public int x;
+    public int y;
+
+    public int gCost;
+    public int hCost;
+    public int fCost;
+
+    public PathNode cameFromNode;
+
+
+  public PathNode(GridSystem<PathNode> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void CalculateFCost()
+    {
+        fCost = gCost + hCost;
+    }
+
+    public override string ToString()
+    {
+        return x + "," + y;
+    }
+}
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,24 +1,59 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    Grid grid;
-    int value;
+    private Pathfinding pathfinding;
     private void Start()
     {
-        grid = new Grid(4, 4, 10f, new Vector3(20, 0));
+        pathfinding = new Pathfinding(10, 10);
     }
-
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), 10);
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+            Vector3 mouseWorldPosition = GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
+            List<PathNode> path = pathfinding.FindPath(0, 0, x, y);
+            if (path != null)
+            {
+                for (int i = 0; i < path.Count - 1; i++)
+                { 
+                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, new Vector3(path[i + 1].x, path[i + 1].y) * 10f + Vector3.one * 5f, Color.green, 5f);
+                }
+            }
         }
     }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
 }
+
+    /*    Grid grid;
+        int value;
+        private void Start()
+        {
+            grid = new Grid(4, 4, 10f, new Vector3(20, 0));
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), 10);
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+            }
+        }*/
